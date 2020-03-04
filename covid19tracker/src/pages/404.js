86e3b25b5539b8f8595312bd74fa3a8967933838
Sowmiya Krishnan <sowmiya.krishnan@here.com>
2020-03-04 11:20:24 +0530

    Tracking Coronavirus

---
+++ covid19tracker/src/pages/404.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/Layout"
+import SEO from "../components/SEO"
+
+const NotFoundPage = () => (
+  <Layout>
+    <SEO title="404: Not found" />
+    <h1>404</h1>
+  </Layout>
+)
+
+export default NotFoundPage
